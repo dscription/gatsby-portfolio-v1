d2116de172e2bd8bbe95ae6c29789288c44be073
Dan Boterashvili <dan.boterashvili@gmail.com>
2021-02-18 20:10:22 -0500

    add About section markup first pass

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -3,7 +3,16 @@ import React from "react"
 const About = () => {
   return (
     <>
-      <h1>About</h1>
+      <h1>Who am I?</h1>
+      <p>
+        I am a personal trainer and visual artist turned software engineer. As a
+        trainer I have spent countless hours working with people on a personal
+        level helping them create lasting change in their lives. I am passionate
+        about the intersection between human movement, creative expression, and
+        technology. This experience has taught me to be empathetic, curious, and
+        persistent when it comes to finding solutions to complex problems. I
+        strive to create products and experiences that bring people together.
+      </p>
     </>
   )
 }
