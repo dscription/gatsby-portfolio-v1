4ec8f6cb897ba18635e17faa7c0f01de14262623
Dan Boterashvili <dan.boterashvili@gmail.com>
2021-03-03 22:44:05 -0500

    change sub header text in HeroSection

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -5,12 +5,12 @@ const HeroSection = () => {
     <section>
       {/* Todo: Add headshot image here */}
       <h1>
-        Hi, I'm <strong>Dan Boterashvili</strong>
+        Hi, my name is <strong>Dan Boterashvili</strong>
       </h1>
-      <p>
-        Software Engineer with a passion for creative technology and human
-        movement.
-      </p>
+      <h2>
+        I'm a software engineer based in New York, NY. I have a passion for creative technology, human movement, and health. I specialize in full stack data driven applications.
+      </h2>
+      <button>Contact Me</button>
     </section>
   )
 }
