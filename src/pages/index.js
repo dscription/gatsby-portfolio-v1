e86988c427d6f47ad9c9d4e5af63b3599fc067c9
Dan Boterashvili <dan.boterashvili@gmail.com>
2021-03-03 22:33:00 -0500

    Update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React from "react"
 import HeroSection from "../components/sections/HeroSection"
-import { Link } from "gatsby"
-
+// import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
 import About from "../components/sections/About"
 import Experience from "../components/sections/Experience"
 import FeaturedProjects from "../components/sections/FeaturedProjects"
 import Projects from "../components/sections/Projects"
 import Contact from "../components/sections/Contact"
+import GlobalStyle from "../components/global"
 
 const IndexPage = () => (
   <Layout>
@@ -19,6 +19,7 @@ const IndexPage = () => (
     <FeaturedProjects />
     <Projects />
     <Contact />
+    <GlobalStyle />
   </Layout>
 )
 
