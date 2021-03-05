b3054357616d8b4cb2f17217e0434f79840463ee
Dan Boterashvili <dan.boterashvili@gmail.com>
2021-03-05 13:02:59 -0500

    add contact text

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -4,7 +4,9 @@ import Section from '../layouts/Section'
 const Contact = () => {
   return (
     <Section>
-      <h2>Contact</h2>
+      <h2>Contact Me</h2>
+      <p>I am currently looking for new opportunities. I am always available to explore new ideas and meet new people. Feel free to reach out and I will get back to you promptly.</p>
+      <button>Email Me!</button>
     </Section>
   )
 }
