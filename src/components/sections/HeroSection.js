fe8a879bd247a6421ca65fac85a6a695c866c315
Dan Boterashvili <dan.boterashvili@gmail.com>
2021-03-17 21:55:35 -0400

    fix typo in HeroSection

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -5,12 +5,11 @@ import { Button, Heading, SubHeading, Content } from "../elements"
 const HeroSection = () => {
   return (
     <Section>
-      <SubHeading>Hi, my name is,</SubHeading>
+      <SubHeading>Hi, my name is</SubHeading>
       <Heading>Dan Boterashvili</Heading>
       <Content>
-        I'm a software engineer based in New York, NY. I have a passion for
-        creative technology, human movement, and health. I specialize in full
-        stack data driven applications.
+        I'm a software engineer based in Brooklyn, NY. I have a passion for
+        creative technology, human movement, and health. I specialize in Front End React Development.
       </Content>
       <Button>Contact Me</Button>
     </Section>
